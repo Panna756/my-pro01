3f75d8d1e68835cc10f79e6e64423845f4defb21
G-vagon <pannatatmon@gmail.com>
2025-05-04 22:18:46 +0700

    create env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 // สร้าง connection
 const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Mysql123",
-  database: "myapp",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // promise wrapper
